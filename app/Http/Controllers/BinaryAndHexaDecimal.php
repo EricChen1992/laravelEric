d6add6bf2ca447f004261c33451ee652b2653bc9
EricChen1992 <azxt1468@hotmail.com>
2020-01-05 11:16:02 +0800

    Modify controller_BinartAndHexaoDecimal

--- app/Http/Controllers/BinaryAndHexaDecimal.php
+++ app/Http/Controllers/BinaryAndHexaDecimal.php
@@ -7,8 +7,16 @@ use Illuminate\Http\Request;
 class BinaryAndHexaDecimal extends Controller
 {
     public function doSomthing(Request $request, $decimal, $math = 2){
-        
-        $this->checkType($decimal, $math);
+        /* 傳址 */
+        // $arrayValue = array($decimal,'123');
+        // $this->changeArrayValue($arrayValue);
+        // $this->showArrayValue($arrayValue);
+
+        /*取的?後面的參數*/ 
+        $this->getParameters($request);
+
+        /*1/3 Home Work */
+        $this->checkType($decimal, $math); //check input variable is a number or a numeric string.
 
         switch ($math) {
             case '2':
@@ -27,12 +35,24 @@ class BinaryAndHexaDecimal extends Controller
     }
 
     private function getChange(int $decimal, $math){
-        dd(base_convert($decimal, 10, $math));
+        dd("$decimal 的 $math 進制是 ".base_convert($decimal, 10, $math));
     }
 
     private function checkType($num_decimal, $math_math){
-        
-        if(!is_numeric($num_decimal)) abort(403,"You enter decimal variable is not int Type");
-        if(!is_numeric($math_math)) abort(403,"You enter math variable is not int Type");
+        if (!is_numeric($num_decimal)) abort(403,"You enter decimal variable is not int Type");
+        if (!is_numeric($math_math)) abort(403,"You enter math variable is not int Type");
+    }
+
+    private function getParameters($request){
+        $parameters = $request->get('first');
+        if ($parameters != null ) dump("First = $parameters");
+    }
+
+    private function changeArrayValue(&$arrayValue){
+        $arrayValue[1] = '456';
+    }
+
+    private function showArrayValue($arrayValue){
+        dump("Value[0] = $arrayValue[0] ------ Value[1] = $arrayValue[1]");
     }
 }
